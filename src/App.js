8cb09ee584990cf624c5063f8fe992eb2f5d660a
12nasir <nasirahmed8202@gmail.com>
2022-07-25 10:03:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,64 @@
 import logo from './logo.svg';
-import {useEffect, useRef, useState} from 'react';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
   const [users, setUsers] = useState([]);
+
   const nameRef = useRef();
   const emailRef = useRef();
+
 
   useEffect( () =>{
     fetch('http://localhost:5000/users')
     .then(res => res.json())
     .then(data => setUsers(data))
-  },[])
+  }, [])
 
-  const handleUser = e =>{
+  
+  // Send data to the server
+  const handleSubmit = e =>{
+
     const name = nameRef.current.value;
     const email = emailRef.current.value;
-    const newUser = {name: name, email: email};
-
-    // send data to the server
+    const newUser = {name: name, email: email}
+    
     fetch('http://localhost:5000/users', {
-      method: 'post',
+      method: 'POST',
       headers: {
-        'content-Type': 'application/json'
-      }, 
+        'content-type': 'application/json'
+      },
       body: JSON.stringify(newUser)
     })
-    .then(res =>res.json())
+    .then(res => res.json())
     .then(data =>{
-      console.log(data);
       const addedUser = data;
-      const newUsers = [...users,addedUser];
+      const newUsers = [...users, addedUser];
       setUsers(newUsers);
     })
 
     nameRef.current.value = '';
     emailRef.current.value = '';
+
     e.preventDefault()
   }
+ 
 
   return (
     <div className="App">
-      <h3>Found data: {users.length}</h3>
+      <h4>Total users: {users.length}</h4>
 
-      <form onSubmit={handleUser}>
-        <input type="text" ref={nameRef} placeholder='name'/>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={nameRef} placeholder='name' />
+        <br />
         <input type="email" ref={emailRef} name="" id="" placeholder='email' />
+        <br />
         <input type="submit" value="Submit" />
       </form>
 
       <ul>
         {
-          users.map(user =><li
-          key={user.id}
-          >{user.id}. {user.name}: 
-          {user.email}</li>)
+          users.map(user => <li key={user.id}>{user.id} {user.name} {user.email}</li>)
         }
       </ul>
     </div>
